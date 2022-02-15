77091f665c681b1edf6f81d9f3f9e1a7ede33b45
vghuser <vghuser@DESKTOP-1765SNU.vghtpe.gov.tw>
2022-02-15 11:36:45 +0800

    修改TwoSumSolution code

--- LeetCodeProject/src/TwoSumSolution.java
+++ LeetCodeProject/src/TwoSumSolution.java
@@ -1,11 +1,12 @@
 import java.util.HashMap;
 
+//001
 public class TwoSumSolution {
 	
 	public static void main(String[] args) 
 	{
-		int[] numbers = {2,7,11,15};
-		int[] ans = twoSum(numbers, 9);
+		int[] numbers = {3,2,4};
+		int[] ans = twoSum(numbers, 5);
 		for(int tmp: ans)
 		{
 			System.out.println(tmp);
@@ -13,6 +14,8 @@ public class TwoSumSolution {
 		
 	}
 
+	//此方法為O(n) 將差值存在HashMap中 因此只要sort一遍就可以得到答案
+	
 	 public static int[] twoSum(int[] numbers, int target) {
 	        
 	        HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
